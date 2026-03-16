13ec41217845b7636b34db3912ff5c5914c2ec20
Lucifer-Jimmy <lucifer.stars@qq.com>
2026-03-16 11:28:47 +0800

    init_repo

---
+++ src/main/java/org/jimmyl/ezstat/EzStatApplication.java
@@ -0,0 +1,13 @@
+package org.jimmyl.ezstat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EzStatApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EzStatApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/jimmyl/ezstat/EzStatApplicationTests.java
@@ -0,0 +1,13 @@
+package org.jimmyl.ezstat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EzStatApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
